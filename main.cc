70cd8aed18c133d5753673818b297a75acb57cf3
sksat <sksat@sksat.net>
2019-07-09 19:40:55 +0900

    [add] register

--- main.cc
+++ main.cc
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <cstdint>
 
+#define AX	reg[0]
+#define CX	reg[1]
+#define DX	reg[2]
+#define BX	reg[3]
+#define SP	reg[4]
+#define BP	reg[5]
+#define SI	reg[6]
+#define DI	reg[7]
+
 int main(int argc, char **argv){
 	bool halt_flag = false;
 	uint16_t ip = 0x00;	// プログラムカウンタ
+	uint16_t reg[8];	// レジスタ
 	uint8_t memory[] = {
 		0x90, 0xf4,
 	};
